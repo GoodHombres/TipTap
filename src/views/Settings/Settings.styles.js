7c319e1e97a04e37d0b9e2041c1a0f09f5a6f7cd
Christian Medina <cmedinasoriano@gmail.com>
2018-03-03 18:42:19 -0400

    Added Settings View

--- src/views/Settings/Settings.styles.js
+++ src/views/Settings/Settings.styles.js
@@ -6,16 +6,52 @@ export default StyleSheet.create({
     backgroundColor: '#020202',
     padding: 20,
   },
-  welcome: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: '#4cd964',
-    margin: 10,
+  section: {
+    padding: 10,
   },
-  instructions: {
-    textAlign: 'center',
+  title: {
     color: '#fff',
-    marginBottom: 5,
+    padding: 2,
+    fontWeight: '200',
+    fontSize: 24,
+  },
+  item: {
+    padding: 10,
+    justifyContent: 'space-between',
+    marginBottom: 1,
+    backgroundColor: 'rgba(246, 247, 249, 0.08)',
+  },
+  commonText: {
+    color: '#fff',
+    fontSize: 18,
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  itemButton: {
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  input: {
+    padding: 10,
+    marginRight: 20,
+    borderColor: '#aaa',
+    borderWidth: 1,
+  },
+  addButton: {
+    padding: 20,
+    paddingTop: 10,
+    paddingBottom: 10,
+    backgroundColor: 'rgba(246, 247, 249, 0.08)',
+  },
+  green: {
+    color: '#4cd964',
+  },
+  gray: {
+    color: '#8e8e93',
+  },
+  topSpaced: {
+    paddingTop: 10,
   },
 });

4fa1122b6af64950a903d36570bc885db351393f
Christian Medina <cmedinasoriano@gmail.com>
2018-03-08 20:42:29 -0400

    Add Snackbar and SnackbarDispatcher components
    Snackbar component is a 'dumb' component with animations which relies on its 'smart' container SnackBarDispatcher. SnackbarDispatcher is a component with static calls to dispatch messages as Snackbars.
    
    Closes #23

---
+++ src/components/Snackbar/Snackbar.styles.js
@@ -0,0 +1,32 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    backgroundColor: '#222',
+    position: 'absolute',
+    margin: 10,
+    padding: 20,
+    borderRadius: 10,
+    right: 0,
+    left: 0,
+  },
+  text: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  top: {
+    top: 10,
+  },
+  bottom: {
+    bottom: 10,
+  },
+  success: {
+    color: '#4cd964'
+  },
+  warning: {
+    color: '#f2c94c'
+  },
+  error: {
+    color: '#ff3b30'
+  },
+});

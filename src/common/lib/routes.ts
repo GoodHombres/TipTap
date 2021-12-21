38c185a7061745a84b5e62f682e8b255615e33d5
Erick Josué Sauri <erick@patch.com>
2021-12-20 23:16:42 -0500

    chore(): remove onboarding from nav stack

--- src/common/lib/routes.ts
+++ src/common/lib/routes.ts
@@ -3,15 +3,13 @@ import { NativeStackScreenProps } from '@react-navigation/native-stack';
 export enum Routes {
   Bill = 'Bill',
   Calculator = 'Calculator',
-  Onboarding = 'Onboarding',
   Settings = 'Settings',
 }
 
 export type RootStackParamList = {
-  Bill: undefined;
-  Calculator: undefined;
-  Onboarding: undefined;
-  Settings: undefined;
+  [Routes.Bill]: undefined;
+  [Routes.Calculator]: undefined;
+  [Routes.Settings]: undefined;
 };
 
 export type BillScreenProps = NativeStackScreenProps<
@@ -21,10 +19,6 @@ export type BillScreenProps = NativeStackScreenProps<
 export type CalculatorScreenProps = NativeStackScreenProps<
   RootStackParamList,
   Routes.Calculator
->;
-export type OnboardingScreenProps = NativeStackScreenProps<
-  RootStackParamList,
-  Routes.Onboarding
 >;
 export type SettingsScreenProps = NativeStackScreenProps<
   RootStackParamList,

f0cfc0666c6f8f0b61befc1ebac1457509f8de92
Erick <esauri@users.noreply.github.com>
2024-05-30 19:32:38 -0400

    chore(): configure eslint (#70)

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+// https://docs.expo.dev/guides/using-eslint/
+module.exports = {
+  extends: 'expo',
+  plugins: [
+    'eslint-plugin-react-compiler',
+  ],
+  rules: {
+    'react-compiler/react-compiler': "error",
+  },
+};

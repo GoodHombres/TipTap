0743c4a60daee3b6f5d99325434f874326024dec
Erick Sauri <ericksauri@hey.com>
2024-05-30 19:30:47 -0400

    up

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,9 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
   extends: 'expo',
-  plugins: ['eslint-plugin-react-compiler',],
+  plugins: [
+    'eslint-plugin-react-compiler',
+  ],
   rules: {
     'react-compiler/react-compiler': "error",
   },
